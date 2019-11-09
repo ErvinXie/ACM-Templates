f3b47c95da145b3b3fa83ef5dd508997f89fed30
Ervin <ervinxie@qq.com>
2019-11-09 17:49:18 +0800

    suffix automaton changed

--- templates/字符串/suffix_automaton.cpp
+++ templates/字符串/suffix_automaton.cpp
@@ -1,80 +1,107 @@
-#include <bits/stdc++.h>
-using namespace std;
 
 const int CHARSET = 26;
-struct SAM_MIN {
+const int MAXN = 1e6+5;
+struct SAM{
+    int par[MAXN*2];
+    int go[MAXN*2][CHARSET];
+    int maxl[MAXN*2];
+    int getmin(int x){
+        if(x==0)
+            return 0;
+        return maxl[par[x]]+1;
+    }
+    int epsize[MAXN*2];
+    vector<int> topo;
 
-    struct State {
-        State *par, *go[CHARSET];
-        int maxl;
-        int minl(){
-            return par->maxl+1;
-        }
-        State(int maxl = 0) : par(nullptr), maxl(maxl) {
-            memset(go, 0, sizeof(go));
-        }
-    };
 
-    State *root, *last;
+    int root,last;
+    int size = 0;
+    int newnode(int x = 0){
+        par[size]=-1;
+        memset(go[size], -1, sizeof(go[size]));
+        maxl[size]=x;
+        epsize[size]=0;
 
-    void init(){
-        root = new State();
-        last = root;
+        size++;
+        return size -1;
     }
 
-    void extend(int w) {
-        State *p = last;
+    void init(){
+        size = 0;
+        root = last = newnode();
+    }
 
-        State *np = new State(p->maxl + 1);//next last node
+    void extend(int c){
+        int p = last;
+        int np = newnode(maxl[p]+1);
+        epsize[np] = 1;//一定占用一个ep
 
-        while (p && p->go[w] == nullptr) {
-            p->go[w] = np;
-            p = p->par;
+        while(p!=-1&&go[p][c]==-1){
+            go[p][c] = np;
+            p = par[p];
         }
-        if (p == nullptr) {
-            np->par = root;
-        } else {
-            State *x = p->go[w];
-            if (p->maxl + 1 == x->maxl) {
-                np->par = x;
-            } else {
-                State *nx = new State(p->maxl + 1);
-                memcpy(nx->go, x->go, sizeof(x->go));
-                nx->par = x->par;
-                x->par = nx;
-                np->par = nx;
-                while (p && p->go[w] == x) {
-                    p->go[w] = nx;
-                    p = p->par;
+
+        if(p==-1){
+            par[np] = root;//新点的minl = 1，直接连root
+        }else{
+            int x = go[p][c];
+            if(maxl[x]==maxl[p]+1){
+                par[np]=x;//无需分点
+            }else{
+                int nx = newnode(maxl[p]+1);
+                memcpy(go[nx],go[x], sizeof(go[nx]));
+                par[nx] = par[x];
+                par[x] = par[np] = nx;
+                while(p!=-1&&go[p][c]==x){
+                    go[p][c] = nx;
+                    p = par[p];
                 }
             }
         }
         last = np;
     }
-}sam;
 
-int longest_common_substring(char a[],char b[]){
-    sam.init();
-    int alen = strlen(a),blen = strlen(b);
-    for(int i=0;i<alen;i++){
-        sam.extend(a[i]-'a');
+    int withdraw(int now,int len){
+        while(par[now]!=-1&&maxl[par[now]]>=len)
+            now = par[now];
+        return now;
     }
-    int ans=0;
 
-    auto now = sam.root;
-    int len = 0;
-    for(int i=0;i<blen;i++){
-        if(now->go[b[i]-'a']){
-            now = now->go[b[i]-'a'];
-            len++;
-            ans = max(len,ans);
-        }else{
-            // find the parent node, throw away some of the prefix
-            while(now!=sam.root){
-                now=now->par;
-                len = now->maxl;
+    void toposort(){
+        static int buf[MAXN+1];
+        memset(buf,0, sizeof(buf));
+        int maxmax=0;
+        for(int i=0;i<size;i++){
+            buf[maxl[i]]++;
+            maxmax = max(maxmax,maxl[i]);
+        }
+        for(int i=1;i<=maxmax;i++){
+            buf[i]=buf[i-1]+buf[i];
+        }
+        topo.resize(size);
+        for(int i=0;i<size;i++){
+            topo[--buf[maxl[i]]] = i;
+        }
+    }
+    void getepsize(){
+        toposort();
+        for(int i=size-1;i>=0;i--){
+            int p = topo[i];
+            epsize[par[p]]+=epsize[p];
+        }
+    }
+    void debug(){
+        for(int i=0;i<size;i++){
+            printf("id:%d par:%d max:%d min:%d endpos size:%d\n",i,par[i],maxl[i],getmin(i),epsize[i]);
+            for(int j=0;j<CHARSET;j++){
+                bool ne = false;
+                if(go[i][j]!=-1){
+                    ne = true;
+                    printf("(-%c-> %d) ",j+'a',go[i][j]);
+                }
+                if(ne)
+                    printf("\n");
             }
         }
     }
-    return ans;
-}
+}sam;
